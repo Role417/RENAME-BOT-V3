8d48582f2c36280a128a5f58715457ec5864a23e
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-06-22 14:22:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     # pyro client config
     API_ID = os.environ.get("API_ID", "12618934")  # ⚠️ Required
     API_HASH = os.environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")  # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")  # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6787206224:AAGuxnEDSwOp6CZdMNo_YqtpTiONW6K39VY")  # ⚠️ Required
 
     # premium 4g renaming client
     STRING_API_ID = os.environ.get("STRING_API_ID", "")
@@ -17,8 +17,8 @@ class Config(object):
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
     # database config
-    DB_NAME = os.environ.get("DB_NAME", "renamebotv1")
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://renamebotv1:renamebotv1@cluster0.wpytt6b.mongodb.net/")  # ⚠️ Required
+    DB_NAME = os.environ.get("DB_NAME", "renamebotv3")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://renamebotv3:renamebotv3@cluster0.cxxhmsy.mongodb.net/")  # ⚠️ Required
 
     # other configs
     BOT_UPTIME = time.time()
